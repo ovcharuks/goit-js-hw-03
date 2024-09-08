9b90484e1011d3da687b24dafd75445d78e40c8b
Сергій Овчарук <ovcharuksvg@gmail.com>
2024-09-08 11:41:41 +0300

    1

--- js/task-3.js
+++ js/task-3.js
@@ -1,22 +1,15 @@
-function checkForSpam(message) {
-    const lowerMessage = message.toLowerCase();
-    const includeSpam = lowerMessage.includes("spam");
-    const includeSale = lowerMessage.includes("sale");
-
-    if (includeSpam) {
-        return includeSpam;
-    } else if(includeSale){
-        return includeSale;
+function filterArray(numbers, value) {
+  const newArray = [];
+  for (const item of numbers) {
+    if (item > value) {
+      newArray.push(item);
     }
-    else {return false}
+  }
+  return newArray;
 }
 
-console.log(checkForSpam("Latest technology news")); // false
-console.log(checkForSpam("JavaScript weekly newsletter")); // false
-console.log(checkForSpam("Get best sale offers now!")); // true
-console.log(checkForSpam("Amazing SalE, only tonight!")); // true
-console.log(checkForSpam("Trust me, this is not a spam message")); // true
-console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
-console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
-
-
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
